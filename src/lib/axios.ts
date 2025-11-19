e5a9806c68b39ee7706bef7f6e8b87f460f5f64e
Piyush Kumar <piyush2004.dev@gmail.com>
2025-11-20 01:51:04 +0530

    refactor: remove default headers from axios instance

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 export default instance;
